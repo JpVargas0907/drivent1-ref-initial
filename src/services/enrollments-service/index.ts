afe99c4e18ccbf507bbc258d0937658c48dbe440
João Paulo <joaopaulo0907@gmail.com>
2023-04-06 00:23:37 -0300

    implementação da rota get address by cpf, faltando apenas a resposta para caso seja mandado um cpf inválido.

--- src/services/enrollments-service/index.ts
+++ src/services/enrollments-service/index.ts
@@ -5,12 +5,23 @@ import addressRepository, { CreateAddressParams } from '@/repositories/address-r
 import enrollmentRepository, { CreateEnrollmentParams } from '@/repositories/enrollment-repository';
 import { exclude } from '@/utils/prisma-utils';
 
-async function getAddressFromCEP() {
-  const result = await request.get(`${process.env.VIA_CEP_API}/37440000/json/`);
-
+async function getAddressFromCEP(cep: string) {
+  const result = await request.get(`${process.env.VIA_CEP_API}/${cep}/json/`);
+  const adressInfo = {
+    logradouro: result.data.logradouro,
+    complemento: result.data.complemento,
+    bairro: result.data.bairro,
+    cidade: result.data.localidade,
+    uf: result.data.uf
+  }
+  
   if (!result.data) {
     throw notFoundError();
+  } else if (result.data.erro === true){
+    return result.data;
   }
+
+  return adressInfo;
 }
 
 async function getOneWithAddressByUserId(userId: number): Promise<GetOneWithAddressByUserIdResult> {
@@ -42,7 +53,7 @@ async function createOrUpdateEnrollmentWithAddress(params: CreateOrUpdateEnrollm
   const address = getAddressForUpsert(params.address);
 
   try {
-    await getAddressFromCEP();
+    await getAddressFromCEP(address.cep);
   } catch {
     throw invalidDataError(['invalid CEP']);
   }
